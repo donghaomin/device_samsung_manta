54cd5e73c6a3b950572f3e73fd7240fc0e7255f5
donghaomin <dhmcharles@gmail.com>
2014-03-29 17:39:38 +0100

    Properly modify device_samsung_manta
    * Swap aosp_manta and full_manta to
        keep the consistency of my device repos...
        and leave aosp_manta untouched.
    * Remove vendor files restriction.
    
    合理修改device_samsung_manta
    
    * 调换aosp_manta和full_manta以便
        保持我的所有设备repo们一致...
        并且不破aosp_manta的处.
    * 去除vendor文件的限制.

--- aosp_manta.mk
+++ aosp_manta.mk
@@ -1,5 +1,4 @@
-#
-# Copyright 2013 The Android Open-Source Project
+# Copyright (C) 2011 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +11,35 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 #
-$(call inherit-product, device/samsung/manta/full_manta.mk)
+# This file is the build configuration for a full Android
+# build for manta hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps). Except for a few implementation
+# details, it only fundamentally contains two inherit-product
+# lines, full and manta, hence its name.
+#
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+        LiveWallpapers \
+        LiveWallpapersPicker \
+        MagicSmokeWallpapers \
+        VisualizationWallpapers \
+        librs_jni
+
+PRODUCT_PROPERTY_OVERRIDES := \
+        net.dns1=8.8.8.8 \
+        net.dns2=8.8.4.4
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, device/samsung/manta/device.mk)
 
 PRODUCT_NAME := aosp_manta
+PRODUCT_DEVICE := manta
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on Manta
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_RESTRICT_VENDOR_FILES := false
--- full_manta.mk
+++ full_manta.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2011 The Android Open Source Project
+#
+# Copyright 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,35 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 #
-# This file is the build configuration for a full Android
-# build for manta hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps). Except for a few implementation
-# details, it only fundamentally contains two inherit-product
-# lines, full and manta, hence its name.
-#
-
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-        LiveWallpapers \
-        LiveWallpapersPicker \
-        MagicSmokeWallpapers \
-        VisualizationWallpapers \
-        librs_jni
-
-PRODUCT_PROPERTY_OVERRIDES := \
-        net.dns1=8.8.8.8 \
-        net.dns2=8.8.4.4
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, device/samsung/manta/device.mk)
+$(call inherit-product, device/samsung/manta/aosp_manta.mk)
 
 PRODUCT_NAME := full_manta
-PRODUCT_DEVICE := manta
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Manta
-PRODUCT_MANUFACTURER := Samsung
-PRODUCT_RESTRICT_VENDOR_FILES := owner path
